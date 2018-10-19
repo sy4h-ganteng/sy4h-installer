426622ed00aa7fdeb4fadf5a47dae96ae1042ede
sy4h-ganteng <44298208+sy4h-ganteng@users.noreply.github.com>
2018-10-19 22:39:14 +0700

    Add files via upload

---
+++ sy4h.sh
@@ -0,0 +1,19 @@
+ #!/bin/bash
+figlet "SY4H"
+
+echo "SY4H-G4NT3N9"
+read -p "SY4H-G4NT3N9" 
+ pkg install python2
+   pip2 install urllib3 chardet certifiidna 
+requests
+
+   pkg install wget
+   pkg  install openssl curl
+   pkg install libcurl 
+   mkdir webdav
+   cd webdav
+   wget https://pastebin.com/raw/HnVyQPtR
+   mv HnVyQPtR webdav.py
+  chmod 777 webdav.py
+
+figlet "SY4H"
